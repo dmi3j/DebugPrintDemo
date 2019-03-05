620119caa8c385a97fe76b6bd2bcb3d7324297a4
Dmitrijs Beloborodovs <dmi3j@me.com>
2019-03-05 18:25:17 +0200

    Empty app added

---
+++ DebugPrintDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//  Copyright © 2019 My Company. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var inputField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func pressMe(_ sender: Any) {
+
+    }
+}
+
